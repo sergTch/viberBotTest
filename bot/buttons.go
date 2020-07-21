3b8ed8d5c4748560adc8cfa1e0d3df0cf1e40f86
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2020-07-21 23:36:26 +0300

    reply

--- bot/buttons.go
+++ bot/buttons.go
@@ -9,5 +9,5 @@ func BuildButton(v *viber.Viber, cols int, rows int, image string, text string, 
 	for _, id := range actionIDs {
 		actBody += "/" + id
 	}
-	return v.NewButton(cols, rows, viber.None, actBody, text, image)
+	return v.NewButton(cols, rows, viber.Reply, actBody, text, image)
 }
