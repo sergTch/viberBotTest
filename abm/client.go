aebc927e4571d307c04829b5dc7efecf9c3633b6
orsenkucher <orsen.kucher@gmail.com>
2020-07-23 00:20:36 +0300

    api: reg-confirm

--- abm/client.go
+++ abm/client.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/sergTch/viberBotTest/data"
 )
@@ -92,5 +93,39 @@ func (c *Client) Register(phone, password, signature string) (smsID int, err err
 	}
 
 	smsID = resp.Data.SMSID
+	return
+}
+
+func (c *Client) Confirm(code string, smsID int) (token string, ok bool, err error) {
+	values := url.Values{}
+	values.Set("code", code)
+	values.Set("sms_id", strconv.Itoa(smsID))
+
+	r, err := c.client.PostForm(c.url("/v2.1/client/registration-confirm"), values)
+	if err != nil {
+		return
+	}
+
+	defer r.Body.Close()
+
+	if r.StatusCode != 201 {
+		err = errors.New("Not 201 status")
+		return
+	}
+
+	var resp struct {
+		Data struct {
+			Token string `json:"token"`
+		} `json:"data"`
+		Success bool `json:"success"`
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&resp)
+	if err != nil {
+		return
+	}
+
+	ok = resp.Success
+	token = resp.Data.Token
 	return
 }
