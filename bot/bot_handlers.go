9ec13125446cc3eff88ccbe549a9120c4bb8919c
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2020-07-24 14:56:33 +0300

    silent

--- bot/bot_handlers.go
+++ bot/bot_handlers.go
@@ -12,7 +12,7 @@ import (
 func MyConversaionStarted(v *viber.Viber, u viber.User, conversationType, context string, subscribed bool, token uint64, t time.Time) viber.Message {
 	fmt.Println("new subscriber", u.ID)
 
-	startB := BuildButton(v, 6, 1, "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1200px-Smiley.svg.png", "", "agr", "qwe")
+	startB := BuildButton(v, 6, 1, "", "СТАРТ", "agr", "qwe")
 	keyboard := v.NewKeyboard("", false)
 	keyboard.AddButtons(*startB)
 	msg := v.NewTextMessage("Приветствуем в програме лояльности ABMLoyalty! Для начала работы нажмите СТАРТ")
