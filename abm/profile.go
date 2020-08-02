0251d4f0a522b3fda6d3860c1863cbe4079b4c7c
orsenkucher <orsen.kucher@gmail.com>
2020-08-02 16:24:06 +0300

    fix: better unmarshalling

--- abm/profile.go
+++ abm/profile.go
@@ -64,10 +64,7 @@ func (p *Profile) readFields(r io.Reader) error {
 				Name     string `json:"name"`
 				Key      string `json:"key"`
 				Required bool   `json:"required"`
-				Schema   []struct {
-					ID    string `json:"id"`
-					Value string `json:"value"`
-				} `json:"items"`
+				Schema   schema `json:"items"`
 			} `json:"fields"`
 		} `json:"data"`
 	}
@@ -87,26 +84,50 @@ func (p *Profile) readFields(r io.Reader) error {
 	fields := map[string]bool{}
 	for _, f := range resp.Data.Fields {
 		fields[f.Key] = f.Required
-		schema := schema{}
-		for _, v := range f.Schema {
-			schema = append(schema, struct {
-				ID   string `json:"id"`
-				Name string `json:"name"`
-			}{ID: v.ID, Name: v.Value})
-		}
-		p.schemas[f.Key] = schema
+		p.schemas[f.Key] = f.Schema
 		fmt.Println("!!! !!!")
 		fmt.Println(f.Schema)
-		fmt.Println(schema)
 	}
 
 	p.Fields = fields
 	return nil
 }
 
-type schema []struct {
+type schema []entry
+type entry struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
+}
+
+func (e *entry) UnmarshalJSON(data []byte) error {
+	var v struct {
+		ID    string `json:"id"`
+		Name  string `json:"name"`
+		Value string `json:"value"`
+	}
+
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+
+	e.Name = v.Name
+	if e.Name == "" {
+		e.Name = v.Value
+	}
+
+	e.ID = v.ID
+	if e.ID == "" {
+		var v struct {
+			ID int `json:"id"`
+		}
+
+		if err := json.Unmarshal(data, &v); err != nil {
+			return err
+		}
+
+		e.ID = fmt.Sprintf("%v", v.ID)
+	}
+	return nil
 }
 
 func (p *Profile) Schema(param string) (s schema, ok bool) {
