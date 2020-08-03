bf4d8f1a4c32a06db4628fd79957d88b2048dcbe
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2020-08-03 12:15:39 +0300

    fields

--- abm/profile2.go
+++ abm/profile2.go
@@ -10,6 +10,72 @@ import (
 	"net/http"
 )
 
+type Item struct {
+	ID    string `json:"id"`
+	Value string `json:"value"`
+}
+
+type Field struct {
+	//Value     interface{} `json:"value"`
+	Name      string `json:"name"`
+	Key       string `json:"key"`
+	FieldType int    `json:"field_type"`
+	DataType  int    `json:"data_type"`
+	Required  bool   `json:"required"`
+	Items     []Item `json:"items"`
+}
+
+type Profile2 struct {
+	Gender     Field
+	Region     Field
+	City       Field
+	Main       map[string]Field
+	Additional map[string]Field
+}
+
+func (c *client) NewProfile2(token string) *Profile2 {
+	prof := Profile2{}
+	err := c.LoadFields(&prof, token)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return &prof
+}
+
+func (c *client) LoadFields(prof *Profile2, token string) (err error) {
+	req, err := http.NewRequest("", c.url("/v2/client/profile-params"), nil)
+	if err != nil {
+		return
+	}
+	req.SetBasicAuth(token, "")
+	r, err := c.client.Do(req)
+	if err != nil {
+		return
+	}
+	if r.StatusCode != 200 {
+		fmt.Println("status: ", r.StatusCode)
+		err = errors.New("Not 200 status")
+		return
+	}
+	//buf := &bytes.Buffer{}
+	//tee := io.TeeReader(r.Body, buf)
+	//bytes, _ := ioutil.ReadAll(tee)
+	var resp struct {
+		Data struct {
+			Fields []Field `json:"fields"`
+		} `json:"data"`
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&resp)
+	if err != nil {
+		return
+	}
+	for _, field := range resp.Data.Fields {
+		prof.Additional[field.Key] = field
+	}
+	return
+}
+
 func (c *client) ProfileLoadTest(token string) (err error) {
 	req, err := http.NewRequest("", c.url("/v2/client/profile-params"), nil)
 	if err != nil {
@@ -30,41 +96,11 @@ func (c *client) ProfileLoadTest(token string) (err error) {
 		return
 	}
 
-	var resp struct {
-		Data map[string]interface{} `json:"data"`
-	}
-
 	buf := &bytes.Buffer{}
 	tee := io.TeeReader(r.Body, buf)
 	bytes, _ := ioutil.ReadAll(tee)
 	fmt.Println(string(bytes))
 	r.Body = ioutil.NopCloser(buf)
-
-	// var v struct {
-	// 	Data struct {
-	// 		Params struct {
-	// 			Vals []struct {
-	// 				ID   string `json:"id"`
-	// 				Name string `json:"value"`
-	// 			} `json:"work_status_params"`
-	// 		} `json:"params"`
-	// 	} `json:"data"`
-	// }
-
-	// var v struct {
-	// 	Data struct {
-	// 		Params map[string]interface{} `json:"params"`
-	// 	} `json:"data"`
-	// }
-
-	//s := v.Data.Params["work_status_params"]
-
-	err = json.NewDecoder(r.Body).Decode(&resp)
-	if err != nil {
-		return
-	}
-
-	fmt.Printf("%+v", resp)
 
 	return nil
 }
