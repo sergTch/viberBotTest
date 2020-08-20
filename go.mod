e59a2d683fff367d9952ff46855272b86d56d321
Arseniy Kucher <orsen.kucher@gmail.com>
2020-08-20 13:22:08 +0300

    gorm

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sergTch/viberBotTest
 go 1.13
 
 require (
+	github.com/jinzhu/gorm v1.9.16
 	github.com/matryer/is v1.4.0
 	github.com/orsenkucher/nothing/encio v0.0.0-20200718092502-89b9762067b4
 	github.com/orsenkucher/viber v0.0.0-20200728183928-2399979e808f
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/orsenkucher/nothing/encio"
 	"github.com/sergTch/viberBotTest/abm"
 	"github.com/sergTch/viberBotTest/bot"
@@ -32,6 +36,12 @@ func run() error {
 
 	key := encio.NewEncIO(*s)
 
+	db, err := NewDB(key)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
 	cfg, err := key.GetConfig("secure/viber.json")
 	if err != nil {
 		return err
@@ -40,7 +50,6 @@ func run() error {
 	v := bot.NewBot(cfg)
 
 	// you really need this only once, remove after you set the webhook
-
 	if *h {
 		hook, err := v.SetWebhook("https://loyalty-vbot.abmloyalty.app/viber/webhook", nil)
 		if err != nil {
@@ -70,4 +79,37 @@ func run() error {
 	}
 
 	return nil
+}
+
+func NewDB(key encio.EncIO) (*gorm.DB, error) {
+	var cfg struct {
+		Driver string `json:"driver"`
+		Host   string `json:"host"`
+		Port   int    `json:"port"`
+		User   string `json:"user"`
+		Pass   string `json:"pass"`
+		Name   string `json:"name"`
+	}
+
+	bytes, err := key.ReadFile("secure/gorm.json")
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bytes, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.User, cfg.Pass, cfg.Name,
+	)
+
+	db, err := gorm.Open(cfg.Driver, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w\n", err)
+	}
+
+	return db, nil
 }
